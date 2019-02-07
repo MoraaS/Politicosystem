afedd89040503869fbe1380f54d9b34ef405d583
John Doe <salmamoraa@gmail.com>
2019-02-07 10:10:58 +0300

    [Starts #163781962] Starts with creating the basetest file

--- test/v1/basetest.py
+++ test/v1/basetest.py
@@ -1,1 +1,20 @@
+'''Entry point for the tests'''
+import json
+import unittest
+from app import app
 
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client(self)
+
+    def get(self, url):
+        return self.client.get(url, content_type="application/json")
+
+    def post(self, url, data):
+        return self.client.post(url,
+                                data=json.dumps(data),
+                                content_type="application/json",)
+
+    def tearDown(self):
+        pass
