09c3a33e13da2cc59e487baaddc207b7017459bc
John Doe <salmamoraa@gmail.com>
2019-02-07 18:21:34 +0300

    [Finishes #163788855] create test_cases for parties

--- test/v1/test_party.py
+++ test/v1/test_party.py
@@ -1,1 +1,41 @@
+from test.v1.basetest import BaseTestCase
+import json
 
+
+class TestPartyCase(BaseTestCase):
+
+    def test_create_party(self):
+        data = {
+            "name": "Wiper",
+            "hqAddress": "Kitui",
+            "logoUrl": "http://sample._url"
+        }
+        response = self.post('/api/v1/parties', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_party_with_empty_data(self):
+        data = {}
+        response = self.post('/api/v1/parties', data=data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.loads(response.data),
+                         {
+            "status": 400,
+            "error": "You have not provided all the fields"
+        })
+
+    def test_get_all_parties(self):
+        response = self.get('/api/v1/parties')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_specific_party(self):
+        response = self.get('/api/v1/parties/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_non_existent_party(self):
+        response = self.get('/api/v1/parties/100')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(json.loads(response.data),
+                         {
+            "status": 404,
+            "error": "The party does not exist"
+        })
