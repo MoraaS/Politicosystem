14f76bfe6567d434ee51a6d967f6ee4fa3773116
John Doe <salmamoraa@gmail.com>
2019-02-13 21:21:54 +0300

    [Finishes #163906100] set up database and endpoint for creating account

---
+++ app/api/v2/models/usermodel.py
@@ -0,0 +1,27 @@
+from app.api.v2.models.dbconfig import Database
+
+
+class UserModel(Database):
+
+    def __init__(self, firstname=None, lastname=None, othername=None, email=None, password=None):
+
+        super().__init__()
+        self.firstname = firstname
+        self.lastname = lastname
+        self.othername = othername
+        self.email = email
+        self.password = password
+
+    def register_user(self, firstname, lastname, othername, email, password):
+        self.curr.execute(
+            '''
+        INSERT INTO users(firstname, lastname, othername, email, password) VALUES
+        ('{}','{}','{}','{}','{}') RETURNING firstname, lastname, othername, email, password'''
+            .format(firstname, lastname, othername, email, password))
+        user = self.curr.fetchone()
+        self.conn.commit()
+        self.curr.close()
+        return user
+
+    # def get_user_by_email(self, email)
+    #    query= "SELECT * FROM users WHERE email="
